8bc1c41041601529720086228a7f965f58e997d8
aisaacroth <aisaacroth@gmail.com>
2015-12-31 17:12:34 -0500

    Finished chocolate feast challenge.

--- Chocolate-Feast/Python/choco_count.py
+++ Chocolate-Feast/Python/choco_count.py
@@ -8,7 +8,28 @@ Date:   2015-12-31
 import sys
 
 def main(args):
-    pass
+    num_tests, tests = get_testcases(args[1])
+    for test in tests:
+        money = test[0]
+        cost = test[1]
+        wrapper_cost = test[2]
+        print(count_chocolate(money, cost, wrapper_cost))
+
+
+def get_testcases(filename):
+    input_file = open(filename, 'r')
+    num_tests = int(input_file.readline())
+    tests = []
+
+    for i in input_file:
+        test_vars = i.split()
+        tests.append((int(test_vars[0]), int(test_vars[1]), int(test_vars[2])))
+    return (num_tests, tests)
+
+
+def count_chocolate(money, cost, wrapper_cost):
+    wrapper_total = money // cost
+    return wrapper_total + (wrapper_total - 1) // (wrapper_cost - 1)
 
 
 def print_arguments(arg):
