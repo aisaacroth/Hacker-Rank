0607237e1c0ebdc2ea6b8181a83d21ab8c6b25f4
aisaacroth <aisaacroth@gmail.com>
2015-12-31 11:10:14 -0500

    More documentaiton.

--- Cut-The-Sticks/Python/cut_sticks.py
+++ Cut-The-Sticks/Python/cut_sticks.py
@@ -18,6 +18,17 @@ def get_testcase():
 
 
 def determine_num_of_cuts(cut_list):
+    '''Determines the number of sticks that need to be cut once the smallest
+    stick has been located.
+    
+    Uses the following algorithm: Once we sort the list, the minimum length
+    stick is at the front of the list. From here, we set that stick as the
+    minimum and step through the list. Once we find a stick that is longer than
+    the minimum, we set that as the new minimum to simulate the stick cutting
+    and then determine how many sticks remain. Since the list is organized in
+    monotonically increasing order, we know that all sticks to the left of the
+    current index will be zero or negative in value, and thus, do not need to be
+    considered when making feature cuts.'''
     min_cut = -1
     i = 0
 
